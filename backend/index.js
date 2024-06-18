5ee8b59f18668c8858143c6128438bb13b75a037
rohankenjale <rohankenjale50@gmail.com>
2024-06-18 17:45:42 +0530

    cors error resolved

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const mainRouter = require('./routes/index')
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+    origin : ["https://payments-app-backend-three.vercel.app/"]
+    }
+));
 app.use(express.json());
 app.use('/api/v1',mainRouter)
 
