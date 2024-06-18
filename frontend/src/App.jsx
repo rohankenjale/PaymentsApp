946791d49a8fc1ab3906a248d97df7a324818feb
rohankenjale <rohankenjale50@gmail.com>
2024-06-18 17:38:16 +0530

    changed home page to signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
+      <Route path='/' element={<SignUp/>}/>
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
