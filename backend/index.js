92406285a68b45aa8a4e0e83881986aacc2afc40
rohankenjale <rohankenjale50@gmail.com>
2024-06-19 14:09:46 +0530

    cors function

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const cors = require('cors')
 const express = require("express");
 const mainRouter = require('./routes/index')
 const app = express();
-app.use(cors());
-app.enableCors({
+app.use(cors({
     origin: [
       "*"    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH'],
     credentials: true,
-  });
+  }));
+app.options('*', cors())
 app.use(express.json());
 app.use('/api/v1',mainRouter)
 
