0662521936c8221bce5cba17bf64b29de9c90dad
rohankenjale <rohankenjale50@gmail.com>
2024-07-02 17:00:04 +0530

    cors error resolution

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,19 @@
-const cors = require('cors')
 const express = require("express");
+const cors = require('cors')
 const mainRouter = require('./routes/index')
 const app = express();
-app.use(cors({
-    origin: [
-      "*"    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH'],
-    credentials: true,
-  }));
-app.options('*', cors(
-    {
-        origin: [
-          "*"    ],
-        methods: ['GET', 'POST', 'PUT', 'PATCH'],
-        credentials: true,
-      }
-))
+app.use(cors());
+app.options('*',cors());
+
+// app.options('*', cors(
+//     {
+//         origin: [
+//           "*"    ],
+//         methods: ['GET', 'POST', 'PUT'],
+//         credentials: true,
+//         preflightContinue: false
+//       }
+// ))
 app.use(express.json());
 app.use('/api/v1',mainRouter)
 
