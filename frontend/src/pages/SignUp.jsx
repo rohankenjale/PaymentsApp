1f8ff092b2454dfac4dd8f2ecceb3654969550bf
rohankenjale <rohankenjale50@gmail.com>
2024-07-06 15:04:30 +0530

    error text size reduced

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,9 +4,9 @@ import axios from "axios";
 import { z } from "zod";
 
 const signUpSchema = z.object({
-  firstname: z.string().nonempty("First name is required"),
-  lastname: z.string().nonempty("Last name is required"),
-  username: z.string().nonempty("Username is required"),
+  firstname: z.string().min(1,"First name is required"),
+  lastname: z.string().min(1,"Last name is required"),
+  username: z.string().min(1,"Username is required"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
@@ -57,29 +57,29 @@ export const SignUp = () => {
           <div className="my-2">
             <div className="py-1"><label htmlFor="" className="text-white font-semibold">First Name</label></div>
             <input value={firstname} onChange={(val)=>{ setFirstname(val.target.value); setValidationErrors({ ...validationErrors, firstname: null });}} placeholder="firstname" className="w-full bg-transparent border-2 border-gray-300 p-2 rounded-lg text-white"/>
-            {validationErrors.firstname && <span className="text-red-500">{validationErrors.firstname._errors[0]}</span>}
+            {validationErrors.firstname && <span className="text-red-500 text-xs">{validationErrors.firstname._errors[0]}</span>}
           </div>
           <div className="my-2">
             <div className="py-1">
               <label htmlFor="" className="text-white font-semibold">Last Name</label>
             </div>
             <input value={lastname} onChange={(val) => { setLastname(val.target.value); setValidationErrors({ ...validationErrors, lastname: null }); }} placeholder="lastname" className="w-full bg-transparent border-2 border-gray-300 p-2 rounded-lg text-white"/>
-            {validationErrors.lastname && <span className="text-red-500">{validationErrors.lastname._errors[0]}</span>}
+            {validationErrors.lastname && <span className="text-red-500 text-xs ">{validationErrors.lastname._errors[0]}</span>}
           </div>
           <div className="my-2">
             <div className="py-1"><label htmlFor="" className="text-white font-semibold">User Name</label></div>
             <input value={username} onChange={(val)=>{ setUsername(val.target.value); setValidationErrors({ ...validationErrors, username: null }); }} placeholder="username" className="w-full bg-transparent border-2 border-gray-300 p-2 rounded-lg text-white"/>
-            {validationErrors.username && <span className="text-red-500">{validationErrors.username._errors[0]}</span>}
+            {validationErrors.username && <span className="text-red-500 text-xs">{validationErrors.username._errors[0]}</span>}
           </div>
           <div className="my-2">
             <div className="py-1">
               <label htmlFor="" className="text-white font-semibold">Password</label>
             </div>
             <input value={password} onChange={(val)=>{ setPassword(val.target.value); setValidationErrors({ ...validationErrors, password: null }); }} placeholder="password" className="w-full bg-transparent border-2 border-gray-300 p-2 rounded-lg text-white"/>
-            {validationErrors.password && <span className="text-red-500">{validationErrors.password._errors[0]}</span>}
+            {validationErrors.password && <span className="text-red-500 text-xs">{validationErrors.password._errors[0]}</span>}
           </div>
           <button type="submit" className="my-3.5 w-full text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign Up</button>
-          {error && <div className="text-red-500 text-center">{error}</div>}
+          {error && <div className="text-red-500 text-center text-xs">{error}</div>}
           <div className="text-white text-center">
             Already have an account? Login
           </div>
